271665fbe4f57e5b39264849f250755fbba476e0
Pranathi-29 <39451274+Pranathi-29@users.noreply.github.com>
2025-12-02 22:28:24 +0530

    Time: 18 ms (82.52%) | Memory: 59.7 MB (59.22%) - LeetSync

--- LinkedList/2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
+++ LinkedList/2263-maximum-running-time-of-n-computers/maximum-running-time-of-n-computers.cpp
@@ -1,15 +1,24 @@
 class Solution {
 public:
-    long long maxRunTime(int n, vector<int>& arr) {
-        sort(arr.begin(), arr.end());
-        long long total = accumulate(arr.begin(), arr.end(), 0LL);
+    long long maxRunTime(int n, vector<int>& batteries) {
+        long long total = 0;
+        for (long long b : batteries) total += b;
 
-        for (int i = arr.size() - 1; i >= 0; i--) {
-            if (arr[i] <= total / n) break;
-            total -= arr[i];
-            n--;
+        long long left = 0, right = total / n;
+
+        while (left < right) {
+            long long mid = (left + right + 1) / 2;
+
+            long long power = 0;
+            for (long long b : batteries)
+                power += min(b, mid);
+
+            if (power >= mid * n)
+                left = mid;
+            else
+                right = mid - 1;
         }
 
-        return total / n;
+        return left;
     }
 };
